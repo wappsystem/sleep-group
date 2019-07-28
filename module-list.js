21cc7513decb9dff1f2f0d9c7d32a121931e7dd2
Gunnar Unger <gunnar.unger@gmail.com>
2019-07-29 09:33:02 +1000

    update

--- module-list.js
+++ module-list.js
@@ -1,10 +1,11 @@
 (function(){
     //-------------------------------------------------------------------------------------
     var modules={
-        "paid-data":  			{url:"$H/modules/paid-data.html",Table:"paid-wappsystem",form_module:"paid-form",tags:"marketing"},
-        "paid-form":  			{url:"$H/modules/paid-form.html",Table:"paid-wappsystem",tags:'marketing'},
-        "charged-data":    			    {url:"$H/modules/charged-data.html",Table:"charged-wappsystem",form_module:"charged-form"},
-        "charged-form":    			    {url:"$H/modules/charged-form.html",Table:"charged-wappsystem"},
+        "dashboard":  			{url:"$H/m/dashboard.html"},
+        "paid-data":  			{url:"$H/m/paid-data.html",Table:"paid-wappsystem",form_module:"paid-form",tags:"marketing"},
+        "paid-form":  			{url:"$H/m/paid-form.html",Table:"paid-wappsystem",tags:'marketing'},
+        "charged-data":    			    {url:"$H/m/charged-data.html",Table:"charged-wappsystem",form_module:"charged-form"},
+        "charged-form":    			    {url:"$H/m/charged-form.html",Table:"charged-wappsystem"},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p]; 
