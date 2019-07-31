0ad6db3d66806320430deb5511676e2f48d6a33b
Gunnar Unger <gunnar.unger@gmail.com>
2019-08-01 08:47:45 +1000

    update

--- module-list.js
+++ module-list.js
@@ -1,11 +1,20 @@
 (function(){
     //-------------------------------------------------------------------------------------
     var modules={
+        "special":  			{url:"$H/m/special.html"},
         "dashboard":  			{url:"$H/m/balance-current.html",Table:"charged-wappsystem",Table_2:"paid-wappsystem"},
         "paid-data":  			{url:"$H/m/paid-data.html",Table:"paid-wappsystem",form_module:"paid-form",tags:"marketing"},
         "paid-form":  			{url:"$H/m/paid-form.html",Table:"paid-wappsystem",tags:'marketing'},
         "charged-data":    			    {url:"$H/m/charged-data.html",Table:"charged-wappsystem",form_module:"charged-form"},
         "charged-form":    			    {url:"$H/m/charged-form.html",Table:"charged-wappsystem"},
+        "b-ftt12-data":    			    {url:"$H/m/ftt12-data.html",Table:"b-ftt12-wappsystem",form_module:"b-ftt12-form"},
+        "b-ftt12-form":    			    {url:"$H/m/ftt12-form.html",Table:"b-ftt12-wappsystem"},
+        "b-ftt6-data":    			    {url:"$H/m/ftt6-data.html",Table:"b-ftt6-wappsystem",form_module:"b-ftt6-form"},
+        "b-ftt6-form":    			    {url:"$H/m/ftt6-form.html",Table:"b-ftt6-wappsystem"},
+        "t-ftt12-data":    			    {url:"$H/m/ftt12-data.html",Table:"t-ftt12-wappsystem",form_module:"t-ftt12-form"},
+        "t-ftt12-form":    			    {url:"$H/m/ftt12-form.html",Table:"t-ftt12-wappsystem"},
+        "t-ftt6-data":    			    {url:"$H/m/ftt6-data.html",Table:"t-ftt6-wappsystem",form_module:"t-ftt6-form"},
+        "t-ftt6-form":    			    {url:"$H/m/ftt6-form.html",Table:"t-ftt6-wappsystem"},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p]; 
